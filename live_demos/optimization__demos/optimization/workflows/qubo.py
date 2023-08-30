82f8a150430de63d1ec84d8ce6daab615d27887d
Paul Nation <nonhermitian@gmail.com>
2023-08-30 18:31:31 -0400

    Optimization demos

---
+++ live_demos/optimization__demos/optimization/workflows/qubo.py
@@ -0,0 +1,28 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2023
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+QUBO workflow
+"""
+from optimization.passes import (InequalityToEquality, IntegerToBinary, 
+                                 LinearEqualityToPenalty, LinearInequalityToPenalty,
+                                 MaximizeToMinimize)
+from optimization.fulqrum import Workflow
+
+
+def QUBO_transformer():
+    return Workflow([InequalityToEquality(), # Transformation
+                     IntegerToBinary(), # Transformation
+                     LinearEqualityToPenalty(), # Transformation
+                     LinearInequalityToPenalty(), # Transformation
+                     MaximizeToMinimize(), # Transformation
+                    ], name='qubo-transformer')
